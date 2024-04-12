797121a39a25e02283a0073c30c3444e8bc28019
Luiz Arthur <LUIZARTHURWINTER@GMAIL.COM>
2024-04-11 22:58:40 -0300

    estudos do dia 11/04

---
+++ Guanabara-Revisao.js
@@ -0,0 +1,32 @@
+/*num = [5,3,1,2,6,8]
+
+for (let pos = 0; pos < valores.length; pos++){
+    console.log(`A posição ${pos} tem o valor ${valores[pos]}`)
+}
+
+for (let pos in num) {
+    console.log(num[pos]);
+}
+
+let index = num.indexOf(3) //para achar a posição de um valor específico (se o retorno for -1 ele nao existe)
+
+if (index < 1 ){
+    console.log(`O valor não existe na array`)
+}
+
+else {
+    console.log(`O valor está na posição ${index}`)
+}
+
+console.log('Tudo bem?');
+
+
+
+while //Serve para executar um código a partir de uma condição
+
+do {
+
+} while(); // Serve para executar um código e depois verificar se uma condição é verdadeira ou não
+
+*/
+
